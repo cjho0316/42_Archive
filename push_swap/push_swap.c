6b16705c746cc320957db209e81980bfb0cabe88
cjho0316 <83046766+cjho0316@users.noreply.github.com>
2022-12-30 16:04:05 +0900

    main change

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -6,12 +6,13 @@
 /*   By: jang-cho <jang-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/17 17:44:38 by jang-cho          #+#    #+#             */
-/*   Updated: 2022/12/30 15:59:53 by jang-cho         ###   ########.fr       */
+/*   Updated: 2022/12/30 16:03:33 by jang-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // 받아온 인자에 대해서 정렬을 수행한다. -> algorithmns
 // 인자를 파싱시켜주는 작업해야함.. 인자에 있는 요소들 받아서 배열로 가져다주기
+// system("leaks push_swap");
 #include <stdio.h>
 #include "deque.h"
 
@@ -29,5 +30,5 @@ int	main(int ac, char **av)
 	indexing_all(deq1);
 	sorting_deque(deq1, deq2);
 	free_all(deq1, deq2);
-	system("leaks push_swap");
+	return (0);
 }
