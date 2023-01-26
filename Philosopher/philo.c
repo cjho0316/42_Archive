ba07959d16b7d93605aecee04568937a5e40d765
cjho0316 <83046766+cjho0316@users.noreply.github.com>
2023-01-26 19:09:09 +0900

    philo day2

--- Philosopher/philo.c
+++ Philosopher/philo.c
@@ -6,7 +6,7 @@
 /*   By: jang-cho <jang-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 12:16:46 by jang-cho          #+#    #+#             */
-/*   Updated: 2023/01/25 18:44:40 by jang-cho         ###   ########.fr       */
+/*   Updated: 2023/01/26 19:07:57 by jang-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,6 +36,8 @@ int main(int ac, char **av)
 	return (0);
 }
 
+/* validity check */
+
 int is_valid_arg(t_info info, char **av)
 {
 	info.num_philo = ft_atoi(av[1]);
@@ -45,9 +47,11 @@ int is_valid_arg(t_info info, char **av)
 	if (info.num_philo <= 0 || info.time_to_die < 0 || info.time_to_eat < 0 || info.time_to_sleep < 0)
 		return (-1);
 	if (av[5] != NULL && ft_atoi(av[5]) > 0)
-		info.num_of_philo_must_eat = ft_atoi(av[5]);
+		info.philo_must_eat = ft_atoi(av[5]);
 	return (0);
 }
+
+/* initializing */
 
 int philo_init(t_philo **phil, t_info info)
 {
@@ -55,19 +59,111 @@ int philo_init(t_philo **phil, t_info info)
 
 	i = -1;
 	*phil = malloc(sizeof(t_philo) * info.num_philo);
+	if (phil == 0)
+		return (-2);
 	while (++i < info.num_philo)
 	{
+		//(*phil)[i].arg = arg;
 		(*phil)[i].id = i;
 		(*phil)[i].eat_count = 0;
-		(*phil)[i].left_fork = i;
-		(*phil)[i].right_fork = (i + 1) % info.num_philo;
-		(*phil)[i].last_eat_time = 0;
+		(*phil)[i].left = i;
+		(*phil)[i].right = (i + 1) % info.num_philo;
+		(*phil)[i].last_eat_time = ft_gettime();
 	}
-	mutex_init();
+	return (0);
 }
 
-int mutex_init(t_philo **philo)
+int print_forks_mutex_init(t_info info)
 {
-	pthread_mutex_init(, NULL, )
+	int i;
 
+	i = -1;
+	pthread_mutex_init(&(info.print), NULL);
+	info.forks = malloc(sizeof(pthread_mutex_t) * info.num_philo);
+	if (info.forks == 0)
+		return (-2);
+	while (++i < info.num_philo)
+	{
+		pthread_mutex_init(&(info.forks[i]), NULL);
+	}
+	return (0);
+}
+
+int philo_start(t_philo *phil, t_info info)
+{
+	int i;
+
+	i = -1;
+	while (++i < info.num_philo)
+		pthread_create(&(phil[i].thread), NULL, philo_thread, &(phil[i]));
+	philo_monitoring(phil, info);
+	i = -1;
+	while (++i < info.num_philo)
+		pthread_join(&(phil[i].thread), NULL);
+	return (0);
+}
+
+/* philo thread run */
+
+int philo_thread(t_philo *phil, t_info info)
+{
+	pthread_mutex_lock(&(info.forks[phil->id]));
+	ft_mutex_print(info, phil->id, "has taken a left fork");
+	//
+	pthread_mutex_lock(&(info.forks[(phil->id + 1) % info.num_philo]));
+	ft_mutex_print(info, phil->id, "has taken a right fork");
+	ft_mutex_print(info, phil->id, "is eating");
+	phil->eat_count++;
+	phil->last_eat_time = ft_gettime();
+	ft_intermission(phil->last_eat_time);
+	pthread_mutex_unlock(&(info.forks[(phil->id + 1) % info.num_philo]));
+	//
+	pthread_mutex_unlock(&(info.forks[phil->id]));
+	return (0);
+}
+
+int philo_monitoring(t_philo *phil, t_info info)
+{
+	if (phil)
+}
+
+/* utils */
+
+int ft_gettime(void)
+{
+	struct timeval 	tv1;
+	long long		time;
+	
+	gettimeofday(&tv1, NULL);
+	time = tv1.tv_sec % 10000 * 1000000 + tv1.tv_usec;
+	return (time);
+}
+
+int ft_intermission(long long wait_time)
+{
+	long long		past;
+	long long		pres;
+
+	past = ft_gettime();
+	while (pres-past > wait_time)
+	if ()
+		break;
+
+	return (time);
+}
+
+int ft_mutex_print(t_info info, int id,char *str)
+{
+	long long cur;
+	
+	cur = ft_gettime();
+	pthread_mutex_lock(&(info.print));
+	if (info.finish != 1)
+		printf("%lld, %d, %s\n",(cur-info.start_time), id + 1, str);
+	pthread_mutex_unlock(&(info.print));
+	return (0);
+}
+
+void ft_free_all()
+{
 }
--- Philosopher/philo.h
+++ Philosopher/philo.h
@@ -6,7 +6,7 @@
 /*   By: jang-cho <jang-cho@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/20 12:05:35 by jang-cho          #+#    #+#             */
-/*   Updated: 2023/01/25 18:40:14 by jang-cho         ###   ########.fr       */
+/*   Updated: 2023/01/26 18:35:20 by jang-cho         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,35 +25,30 @@ typedef struct s_time{
 	suseconds_t tv_usec;
 }	t_time;
 
-typedef struct s_info{
-	int num_philo;
-	int time_to_die;
-	int time_to_eat;
-	int time_to_sleep;
-	int num_of_philo_must_eat;
-} 	t_info;
-
-typedef struct  s_philosopher
+typedef struct s_philo
 {
-    t_info     		rule_info;
+    // struct s_arg    *arg;
+    pthread_t       thread;
     int             id;
-    int             eat_count;
-    long long       public_time;
-    long long       eating_start_time;
+    int             left;
+    int             right;
     long long       last_eat_time;
-    // pthread_t       thread;
-    pthread_t print;
-    pthread_t left_fork;
-    pthread_t right_fork;
-}	t_philo;
+    int             eat_count;
+}		t_philo;
 
-// typedef struct  s_philosophers_info
-// {
-//     t_philosopher 	*philosopher;
-//     pthread_t 		*threads;
-//     pthread_mutex_t *forks;
-//     pthread_mutex_t mutex_print;
-// }	t_philosophers_info;
+typedef struct s_info
+{
+    int             num_philo;
+    int             time_to_die;
+    int             time_to_eat;
+    int             time_to_sleep;
+    int             philo_must_eat;
+    int             finish;
+    int             finished_eat;
+    long long       start_time;
+    pthread_mutex_t *forks;
+    pthread_mutex_t print;
+}		t_info;
 
 /* philo_util.c */
 int	ft_atoi(const char *str);
