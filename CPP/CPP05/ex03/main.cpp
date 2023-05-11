f755fc82ae19a5847f99bad3bd3d98cd2a8368a1
cjho0316 <cjho03160316@gmail.com>
2023-05-11 16:42:46 +0900

    feat: ex03 implemented

--- CPP/CPP05/ex03/main.cpp
+++ CPP/CPP05/ex03/main.cpp
@@ -1,29 +1,21 @@
 #include "Bureaucrat.hpp"
-#include "AForm.hpp"
-#include "PresidentialPardonForm.hpp"
-#include "RobotomyRequestForm.hpp"
-#include "ShrubberyCreationForm.hpp"
+#include "Intern.hpp"
 
 int	main(void)
 {
+	Intern intern;
+
 	std::cout << "------------------------------------------------------------------------" << std::endl;
 	{
 		try
 		{
-			std::cout << "CASE1: PresidentialPardonForm" << std::endl;
-			{
-				Bureaucrat bureau1("jang-cho", 25);
-				Bureaucrat bureau2("sechung", 5);
-				AForm*	form1 = new PresidentialPardonForm("jincpark");
+			std::cout << "CASE0: InvalidErrorException" << std::endl;
+			Bureaucrat bureau1("jang-cho", 1);
+			AForm* form1 = intern.makeForm("Invalid form", "jincpark");
 
-				std::cout << bureau1 << std::endl;
-				std::cout << bureau2 << std::endl;
-
-				form1->beSigned(bureau1); // if not getSigned throw 'ExecuteNotSignedException'
-				bureau1.signForm(*form1);
-				bureau2.executeForm(*form1);
-				delete form1;
-			}
+			form1->beSigned(bureau1);	// if not getSigned throw 'ExecuteNotSignedException'
+			bureau1.signForm(*form1);
+			bureau1.executeForm(*form1);
 		}
 		catch (const std::exception& e)
 		{
@@ -34,20 +26,30 @@ int	main(void)
 	{
 		try
 		{
-			std::cout << "CASE2: RobotomyRequestForm" << std::endl;
-			{
-				Bureaucrat bureau1("jang-cho", 72);
-				Bureaucrat bureau2("sechung", 45);
-	
-				AForm* form1 = new RobotomyRequestForm("jincpark");
-				
-				std::cout << bureau1 << std::endl;
-				std::cout << bureau2 << std::endl;
-				form1->beSigned(bureau1);	// if not getSigned throw 'ExecuteNotSignedException'
-				bureau1.signForm(*form1);
-				bureau2.executeForm(*form1);
-				delete form1;
-			}
+			std::cout << "CASE1: ShrubberyCreationForm" << std::endl;
+			Bureaucrat bureau1("jang-cho", 1);
+			AForm* form1 = intern.makeForm("shrubbery creation", "jincpark");
+
+			form1->beSigned(bureau1);	// if not getSigned throw 'ExecuteNotSignedException'
+			bureau1.signForm(*form1);
+			bureau1.executeForm(*form1);
+		}
+		catch (const std::exception& e)
+		{
+			std::cerr << e.what() << std::endl;
+		}
+	}
+	std::cout << "------------------------------------------------------------------------" << std::endl;
+	{
+		try
+		{		
+			std::cout << "CASE2: Presidential PardonForm" << std::endl;
+			Bureaucrat bureau1("jang-cho", 1);
+			AForm* form1 = intern.makeForm("presidential pardon", "jincpark");
+
+			form1->beSigned(bureau1);	// if not getSigned throw 'ExecuteNotSignedException'
+			bureau1.signForm(*form1);
+			bureau1.executeForm(*form1);	
 		}
 		catch (const std::exception& e)
 		{
@@ -58,20 +60,13 @@ int	main(void)
 	{
 		try
 		{
-			std::cout << "CASE3: ShrubberyCreationForm" << std::endl;
-			{
-				Bureaucrat bureau1("jang-cho", 145);
-				Bureaucrat bureau2("sechung", 137);
-				AForm*	form1 = new ShrubberyCreationForm("jincpark");
+			std::cout << "CASE3: RobotomyRequestForm" << std::endl;
+			Bureaucrat bureau1("jang-cho", 1);
+			AForm* form1 = intern.makeForm("robotomy request", "jincpark");
 
-				std::cout << bureau1 << std::endl;
-				std::cout << bureau2 << std::endl;
-
-				form1->beSigned(bureau1); // if not getSigned throw 'ExecuteNotSignedException'
-				bureau1.signForm(*form1);
-				bureau2.executeForm(*form1);
-				delete form1;
-			}
+			form1->beSigned(bureau1);	// if not getSigned throw 'ExecuteNotSignedException'
+			bureau1.signForm(*form1);
+			bureau1.executeForm(*form1);
 		}
 		catch (const std::exception& e)
 		{
